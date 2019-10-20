848deb3ac72600ac065eb3fedaa4fadd1da60378
AShevtsov <Shevcov1994@mail.ru>
2019-10-20 22:23:12 +0300

    Initial commit.

---
+++ src/main/resources/db/migration/V1.1__insert_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users(name, surname, age) VALUES ('alex', 'shevtsov', 25);
+INSERT INTO users(name, surname, age) VALUES ('anastasia', 'popova', 24);
+INSERT INTO users(name, surname, age) VALUES ('sergey', 'devyatkin', 30);
